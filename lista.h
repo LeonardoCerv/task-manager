8c28656c2d704b11d7ee8d29968ab488159915de
Leonardo cervantes <leocerva29@gmail.com>
2024-06-13 00:18:07 -0600

    se agregaron comentarios para la clase lista

--- lista.h
+++ lista.h
@@ -15,17 +15,22 @@
 
 using namespace std;
 
+// Declaro la clase Usuario
 class Lista {
+
+// Variables de instancia
 private:
     string nombre;
     string descripcion;
     bool status;
     vector<Tarea*> tareas;
+
 public:
-    // Constructors
-    Lista() : nombre(""), descripcion(""), status(false), tareas() {};
+    // Constructores
+    Lista() : nombre(""), descripcion(""), status(false), tareas() {}; // Constructor por default
     Lista(string nombre): nombre(nombre), descripcion(""), status(false), tareas() {};
-    Lista(string nombre, string descripcion): nombre(nombre), descripcion(descripcion), status(false), tareas() {};
+    Lista(string nombre, string descripcion): nombre(nombre), descripcion(descripcion), 
+    status(false), tareas() {};
 
     // Getters
     string getNombre() { return nombre; }
@@ -39,14 +44,22 @@ public:
     void setStatus(bool status) { this->status = status; }
     void setTareas(vector<Tarea*> tareas) {this->tareas = tareas;}
 
-    // Other methods
+    // Metodos
     void mostrarTareas();
     void agregarTarea(Tarea* tarea);
     void eliminarTarea(int index);
 
 };
 
-// Definition of moved methods
+/**
+ * mostrarTareas muestra todas las tareas de la lista
+ *
+ * recorre el arreglo tareas[] y muestra el nombre y la descripción 
+ * de cada tarea en la consola.
+ *
+ * @param
+ * @return
+ */
 void Lista::mostrarTareas(){
     cout<<"Estas son las tareas en la lista "<<nombre<<": "<<endl;
     for (int i=0; i < tareas.size(); i++){
@@ -55,6 +68,15 @@ void Lista::mostrarTareas(){
     }
 }
 
+/**
+ * agregarTarea agrega una tarea a la lista
+ *
+ * recorre el arreglo tareas[] y verifica si la tarea ya existe en la lista
+ * si no existe la agrega al final del arreglo.
+ *
+ * @param Tarea* tarea
+ * @return
+ */
 void Lista::agregarTarea(Tarea* tarea){
     for (Tarea * i: tareas){
         if (i->get_nombre() == tarea->get_nombre() && i->get_descripcion() == tarea->get_descripcion()){
@@ -66,6 +88,15 @@ void Lista::agregarTarea(Tarea* tarea){
     cout<<"Se ha agregado la tarea "<<tareas.back()->get_nombre()<<" a la lista "<<nombre<<endl;
 }
 
+/**
+ * eliminarTarea elimina una tarea de la lista
+ *
+ * recorre el arreglo tareas[] y verifica si la tarea existe en la lista
+ * si existe la elimina del arreglo.
+ *
+ * @param int index
+ * @return
+ */
 void Lista::eliminarTarea(int index){
     if (index >= this->tareas.size()){
         cout<<"No existe una tarea en el index "<<index<<endl;
